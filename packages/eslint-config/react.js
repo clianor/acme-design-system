e4a5a705fef3d484c2c262c47e71da37f5bcdffa
김덕현 <oraclian@estsecurity.com>
2024-03-13 23:17:44 +0900

    themes 패키지 추가 및 tsup 설정 공유하도록 패키지들 변경
    Signed-off-by: 김덕현 <oraclian@estsecurity.com>

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -31,7 +31,13 @@ module.exports = {
       },
     },
   },
-  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js", "**/*.css"],
+  ignorePatterns: [
+    "node_modules/",
+    "dist/",
+    ".eslintrc.js",
+    "**/*.css",
+    "tsup.config.ts",
+  ],
   // add rules configurations here
   rules: {
     "import/no-default-export": "off",
