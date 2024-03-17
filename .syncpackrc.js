d10bc9a6e194ed784d56afe8274f77c6aa1029cc
김덕현 <oraclian@estsecurity.com>
2024-03-17 23:09:53 +0900

    모노레포 내에서 일관된 종속성을 보장하기 위한 Syncpack 셋팅
    Signed-off-by: 김덕현 <oraclian@estsecurity.com>

---
+++ .syncpackrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  dependencyTypes: ['dev', 'prod', 'peer'],
+  semverRange: '^',
+  source: ['package.json', 'apps/*/package.json', 'packages/*/package.json'],
+  versionGroups: [
+    {
+      label:
+        '내부 패키지는 "workspace:*로 고정되어야 합니다. (모든 버전이 허용됨을 의미합니다)',
+      packages: ['**'],
+      dependencies: [
+        '@repo/eslint-config',
+        '@repo/prettier-config',
+        '@repo/tsup-config',
+        '@repo/typescript-config',
+        '@acme/thmes',
+        '@acme/ui',
+      ],
+      dependencyTypes: ['dev', 'prod', 'peer'],
+      pinVersion: 'workspace:*',
+    },
+  ],
+  semverGroups: [
+    {
+      range: '',
+    },
+  ],
+};
