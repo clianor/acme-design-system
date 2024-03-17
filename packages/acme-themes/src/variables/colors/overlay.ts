9beaa36ec68baba34d7c0106aa1d0e55fa4bdcb0
김덕현 <oraclian@estsecurity.com>
2024-03-18 03:10:05 +0900

    @acme/themes에 overlay 토큰 추가
    Signed-off-by: 김덕현 <oraclian@estsecurity.com>

---
+++ packages/acme-themes/src/variables/colors/overlay.ts
@@ -0,0 +1,29 @@
+export const blackAlpha = {
+  1: '#00000003',
+  2: '#00000008',
+  3: '#0000000e',
+  4: '#00000014',
+  5: '#0000001a',
+  6: '#00000022',
+  7: '#0000002b',
+  8: '#00000044',
+  9: '#00000072',
+  10: '#0000007f',
+  11: '#0000009b',
+  12: '#000000df',
+} as const;
+
+export const whiteAlpha = {
+  1: '#ffffff00',
+  2: '#ffffff21',
+  3: '#ffffffb0',
+  4: '#ffffff1a',
+  5: '#ffffff22',
+  6: '#ffffff2b',
+  7: '#ffffff37',
+  8: '#ffffff4f',
+  9: '#ffffff5f',
+  10: '#ffffff74',
+  11: '#ffffffa9',
+  12: '#ffffffeb',
+} as const;
