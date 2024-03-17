8eab2ce8e82e7683b169de23274993dddeb07939
김덕현 <oraclian@estsecurity.com>
2024-03-18 03:22:01 +0900

    @acme/themes typography 토큰 추가
    Signed-off-by: 김덕현 <oraclian@estsecurity.com>

---
+++ packages/acme-themes/src/variables/typography.ts
@@ -0,0 +1,42 @@
+export const fontSize = {
+  1: '.75rem',
+  2: '.875rem',
+  3: '1rem',
+  4: '1.125rem',
+  5: '1.25rem',
+  6: '1.5rem',
+  7: '1.75rem',
+  8: '2.1875rem',
+  9: '3.75rem',
+} as const;
+
+export const fontWeight = {
+  light: 300,
+  regular: 400,
+  medium: 500,
+  bold: 700,
+} as const;
+
+export const lineHeight = {
+  1: '1rem',
+  2: '1.25rem',
+  3: '1.5rem',
+  4: '1.625rem',
+  5: '1.75rem',
+  6: '1.875rem',
+  7: '2.25rem',
+  8: '2.5rem',
+  9: '3.75rem',
+} as const;
+
+export const letterSpacing = {
+  1: '0.0025em',
+  2: '0em',
+  3: '0em',
+  4: '-0.0025em',
+  5: '-0.005em',
+  6: '-0.00625em',
+  7: '-0.0075em',
+  8: '-0.01em',
+  9: '-0.025em',
+} as const;
