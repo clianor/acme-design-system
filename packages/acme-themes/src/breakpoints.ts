6a2ebb7b19b24e7f7e412ce123f15927d86a5a87
김덕현 <oraclian@estsecurity.com>
2024-03-23 21:32:30 +0900

    테마에 breakpoints 추가
    Signed-off-by: 김덕현 <oraclian@estsecurity.com>

---
+++ packages/acme-themes/src/breakpoints.ts
@@ -0,0 +1,7 @@
+export const breakpoints = {
+  xs: '520px',
+  sm: '768px',
+  md: '1024px',
+  lg: '1280px',
+  xl: '1640px',
+} as const;
--- packages/acme-themes/src/index.ts
+++ packages/acme-themes/src/index.ts
@@ -2,4 +2,5 @@ import { vars as themeVars } from './theme.css';
 import type * as variables from './variables';
 
 export * from './color.prop';
+export { breakpoints } from './breakpoints';
 export const vars = themeVars as unknown as typeof variables;
