a8e8c2828cab22aa90255a3e9ae37a17a2f2096c
김덕현 <oraclian@estsecurity.com>
2024-04-21 02:27:18 +0900

    Heading 컴포넌트 스토리북 보강
    Signed-off-by: 김덕현 <oraclian@estsecurity.com>

--- apps/docs/stories/typography/heading.stories.tsx
+++ apps/docs/stories/typography/heading.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { accentColors } from '@acme/themes';
-import { Heading } from '@acme/ui';
+import { Box, Flex, Heading } from '@acme/ui';
 
 import { fontSizeKeys } from '../utils/constants';
 
@@ -82,5 +82,111 @@ type Story = StoryObj<typeof Heading>;
 export const Default: Story = {
   args: {
     children: 'The quick brown fox jumps over the lazy dog.',
+  },
+};
+
+export const AsAnotherElement: Story = {
+  render: (props) => {
+    return (
+      <Flex alignItems="start" flexDirection="column" rowGap={3}>
+        <Heading {...props} as="h1">
+          Level 1
+        </Heading>
+        <Heading {...props} as="h2">
+          Level 2
+        </Heading>
+        <Heading {...props} as="h3">
+          Level 3
+        </Heading>
+      </Flex>
+    );
+  },
+};
+
+export const Size: Story = {
+  render: (props) => {
+    return (
+      <Flex alignItems="start" flexDirection="column" rowGap={3}>
+        {[...new Array(9)].map((_, index) => {
+          return (
+            <Heading
+              {...props}
+              key={index.toLocaleString()}
+              size={(index + 1) as never}
+            >
+              The quick brown fox jumps over the lazy dog
+            </Heading>
+          );
+        })}
+      </Flex>
+    );
+  },
+};
+
+export const Weight: Story = {
+  render: (props) => {
+    return (
+      <Flex alignItems="start" flexDirection="column" rowGap={3}>
+        {(['regular', 'bold'] as const).map((weight) => {
+          return (
+            <Heading {...props} key={weight} weight={weight}>
+              The quick brown fox jumps over the lazy dog
+            </Heading>
+          );
+        })}
+      </Flex>
+    );
+  },
+};
+
+export const Align: Story = {
+  render: (props) => {
+    return (
+      <Flex flexDirection="column" rowGap={3}>
+        <Heading {...props} align="left">
+          Left-aligned
+        </Heading>
+        <Heading {...props} align="center">
+          Center-aligned
+        </Heading>
+        <Heading {...props} align="right">
+          Right-aligned
+        </Heading>
+      </Flex>
+    );
+  },
+};
+
+export const Wrap: Story = {
+  render: (props) => {
+    return (
+      <Flex flexDirection="column" rowGap={3}>
+        {(['nowrap', 'balance', 'pretty'] as const).map((wrap) => {
+          return (
+            <Box key={wrap} maxWidth={50}>
+              <Heading {...props} truncate wrap={wrap}>
+                The quick brown fox jumps over the lazy dog
+              </Heading>
+            </Box>
+          );
+        })}
+      </Flex>
+    );
+  },
+};
+
+export const Color: Story = {
+  render: (props) => {
+    return (
+      <Flex flexDirection="column" rowGap={3}>
+        {accentColors.map((color) => {
+          return (
+            <Heading {...props} key={color} color={color}>
+              The quick brown fox jumps over the lazy dog
+            </Heading>
+          );
+        })}
+      </Flex>
+    );
   },
 };
